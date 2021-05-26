67e248318b29ece1542b387eccabfdc410d0d2b6
juanpchica <juanpchica@hotmail.com>
2021-05-26 15:07:24 -0500

    Layout home components for three columns was created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Leftside from "./Leftside";
+import Main from "./Main";
+import Rightside from "./Rightside";
 
 const Home = () => {
   return (
@@ -12,6 +15,11 @@ const Home = () => {
           moving.
         </p>
       </Section>
+      <Layout>
+        <Leftside />
+        <Main />
+        <Rightside />
+      </Layout>
     </Container>
   );
 };
@@ -48,6 +56,21 @@ const Section = styled.section`
     font-weight: 600;
   }
   @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  /* grid-template-row: auto; */
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
     flex-direction: column;
     padding: 0 5px;
   }
---
+++ src/components/Leftside.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Leftside = () => {
+  return <div>left</div>;
+};
+
+export default Leftside;
